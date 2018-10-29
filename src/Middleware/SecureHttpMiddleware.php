34ddb42f99b8d8bd312438ddfed6684895dfab9f
Ando Suguru <ando_suguru@zelfium.com>
2018-10-29 15:51:44 +0900

    no message

---
+++ src/Middleware/SecureHttpMiddleware.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Middleware;
+
+use Cake\Core\InstanceConfigTrait;
+use Cake\Http\ServerRequest;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * SecureHttp middleware
+ */
+class SecureHttpMiddleware
+{
+    use InstanceConfigTrait;
+
+    protected $_defaultConfig = [
+        'forceSSL' => true,
+    ];
+
+    /**
+     * Constructor.
+     *
+     * @param array $config Settings for the filter.
+     */
+    public function __construct($config = [])
+    {
+        $this->setConfig($config);
+    }
+
+    /**
+     * Invoke method.
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request The request.
+     * @param \Psr\Http\Message\ResponseInterface $response The response.
+     * @param callable $next Callback to invoke the next middleware.
+     * @return \Psr\Http\Message\ResponseInterface A response
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $next)
+    {
+        /**
+         * 変数宣言
+         */
+        $config = $this->getConfig();
+
+        /**
+         * プロキシから受け取ったヘッダーからSSLかどうかを判定するdetectorを追加
+         */
+        $request->trustProxy = true;
+        $callable = function (ServerRequest $request) {
+            return $request->scheme() === 'https';
+        };
+        $request->addDetector('ssl', $callable);
+
+        /**
+         * SSLが強制されていてhttpでアクセスされたらリダイレクト
+         */
+        if ($config['forceSSL'] && !$request->is('ssl')) {
+            $host = $request->host();
+            $target = $request->getRequestTarget();
+            $url = "https://{$host}{$target}";
+
+            return $response->withLocation($url);
+        }
+
+        return $next($request, $response);
+    }
+}
